f36276ad760929b67c84e2ecb341f2436d1d4750
Kiagus Arief Adriansyah <arief@alodokter.com>
2019-10-22 08:52:23 +0700

    using docker managed volume

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# # Docker Managed Volume
-# docker run -d --name mongo -v mongo_data:/data -p 27017:27017 kadriansyah/mongo-4_2_1
+# Docker Managed Volume
+docker run -d --name mongo -v mongo_data:/data -p 27017:27017 kadriansyah/mongo-4_2_1
 
-# Bind Mount Volume
-docker run -d --name mongo -v /Users/kadriansyah/projects/data:/data -p 27017:27017 kadriansyah/mongo-4_2_1
+# # Bind Mount Volume
+# docker run -d --name mongo -v /Users/kadriansyah/projects/data:/data -p 27017:27017 kadriansyah/mongo-4_2_1
